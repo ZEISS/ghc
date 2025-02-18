44a76a15421343312e5306f2c92de4e2225014a5
Sebastian Döll <sebastian@katallaxie.dev>
2025-02-18 09:07:37 +0000

    fix: using rev-parse to git dir

--- pkg/hooks/hooks.go
+++ pkg/hooks/hooks.go
@@ -15,18 +15,13 @@ var hookTemplate = "#!/usr/bin/env -S sh -c 'ghc -c %s -r %s'"
 
 // Path returns the path of the hook
 func Path(ctx context.Context) (string, error) {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return "", err
-	}
-
-	cmd := exec.CommandContext(ctx, "git", "config", "core.hooksPath")
+	cmd := exec.Command("git", "rev-parse", "--git-dir")
 	out, err := cmd.Output()
 	if err != nil {
 		return "", err
 	}
 
-	return filepath.Clean(filepath.Join(cwd, strings.TrimSuffix(conv.String(out), "\n"))), nil
+	return strings.TrimSuffix(conv.String(out), "\n"), nil
 }
 
 // Install installs the hook
